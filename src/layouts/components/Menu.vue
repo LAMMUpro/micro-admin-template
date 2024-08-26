fc70b2465b52a45c6469e4c717be37da8a1748e9
陈永超 <chen_yongchao@126.com>
2024-08-26 11:15:45 +0800

    chore: 菜单类型优化、主应用菜单路由跳转

--- src/layouts/components/Menu.vue
+++ src/layouts/components/Menu.vue
@@ -28,52 +28,68 @@ const route = useRoute();
 const router = useRouter();
 
 /**
- * 菜单切换
+ * 菜单切换，路由跳转
  * @param key 菜单唯一标识, 例如: 0-2-1
  */
 function handleMenuChange(key: string) {
+  /**
+   * 提取菜单信息
+   */
   let menuInfo: MenuItemType | undefined;
   key.split('-').forEach((index) => {
     if (menuInfo) {
-      menuInfo = menuInfo.children?.[+index] || ({} as MenuItemType);
+      menuInfo = menuInfo.children?.[+index];
     } else {
       menuInfo = Global.menu.info.menus[+index];
     }
   });
-  if (!menuInfo?.path) return;
 
-  /** 子应用前缀(目标) */
-  const subAppPrefix_target = getSubAppPrefixFromRouteUrl(menuInfo.path);
+  if (!menuInfo) return;
 
-  /** 子应用名称(目标) */
-  const subAppName_target = MicroAppConfig.subAppSettingList.find(
-    (item) => item.prefix === subAppPrefix_target
-  )?.name;
-
-  if (!subAppName_target) return console.error(`未配置${subAppPrefix_target}`);
-
-  /** 子应用名称(当前), 从route.path取不准确(兜底路由情况)需要从route.name取 */
-  const subAppName_current = route.name
-    ?.toString()
-    .startsWith(CONSTS.subAppRouteNamePrefix)
-    ? route.name?.toString().replace(CONSTS.subAppRouteNamePrefix, '')
-    : '';
-
-  if (subAppName_target === subAppName_current) {
-    /**
-     * 当前子应用路由切换,只需要改subAppPath
-     */
-    subAppPath.value = menuInfo.path;
-  } else {
-    /**
-     * 目标路由是其他子应用
-     */
+  if (menuInfo.targetType === 0) {
     router.push({
-      path: `/${subAppName_target}`,
-      query: {
-        [subAppName_target]: menuInfo.path,
-      },
+      path: menuInfo?.path,
     });
+  } else if (menuInfo.targetType === 1) {
+    /** 子应用前缀(目标) */
+    const subAppPrefix_target = getSubAppPrefixFromRouteUrl(menuInfo.path);
+
+    /** 子应用名称(目标) */
+    const subAppName_target = MicroAppConfig.subAppSettingList.find(
+      (item) => item.prefix === subAppPrefix_target
+    )?.name;
+
+    if (!subAppName_target) return console.error(`未配置${subAppPrefix_target}`);
+
+    /** 子应用名称(当前), 从route.path取不准确(兜底路由情况)需要从route.name取 */
+    const subAppName_current = route.name
+      ?.toString()
+      .startsWith(CONSTS.subAppRouteNamePrefix)
+      ? route.name?.toString().replace(CONSTS.subAppRouteNamePrefix, '')
+      : '';
+
+    if (subAppName_target === subAppName_current) {
+      /**
+       * 当前子应用路由切换,只需要改subAppPath
+       */
+      subAppPath.value = menuInfo.path;
+    } else {
+      /**
+       * 目标路由是其他子应用
+       */
+      router.push({
+        path: `/${subAppName_target}`,
+        query: {
+          [subAppName_target]: menuInfo.path,
+        },
+      });
+    }
+  } else if (menuInfo.targetType === 2) {
+    if (menuInfo.openMode == 0) {
+      // 本窗口打开外链
+    } else if (menuInfo.openMode == 1) {
+      // 新窗口打开外链
+    }
   }
 }
 </script>
--- src/types/common.ts
+++ src/types/common.ts
@@ -2,54 +2,103 @@ import { RouteRecordRaw } from 'vue-router';
 
 /** 后端菜单配置 */
 export type MenuOriginType = {
-  /** 菜单id */
-  id: string;
   /** 父级菜单id */
   parentId: string;
   /** 菜单子路由 */
-  children?: Array<MenuItemType>;
-  /** 资源类型：1-菜单，2-按钮 */
-  resourceType: 1 | 2;
+  children?: Array<MenuOriginType>;
   /** 菜单名称(中文) */
   name: string;
+
+  /** 排序, 越小越靠前 */
+  sort: number;
+  /** 是否显示菜单 */
+  hidden: boolean;
+
+  /** 菜单id */
+  id: string;
   /**
-   * 路径(不包含name)
+   * 路径(不包含name)(后端添加的字段)
    * @example /业务中心后台管理/流程管理
    */
   prefixName: string;
-  /** 未激活图标(http url) */
-  icon: string;
-  /**
-   * 路径(不一定对应vue文件路径)
-   * @example /admin/#/xxx
-   * @example 外链：http://xxx.xxx.com/xxx
-   */
-  path: string;
-  /**
-   * vue文件相对路径
-   * @example Layout
-   * @example /clock/xxx/report
-   */
-  componentStr: string;
-  /** 排序, 越小越靠前 */
-  sort: number;
-  /** 是否显示在菜单 */
-  hidden: boolean;
-};
+
+  // /** 目标类型：0-主应用, 1-子应用, 2-外链, 3-父级菜单, 4-权限按钮 */
+  // targetType: 0 | 1 | 2 | 3;
+  // /** 外链打开方式: 0-本窗口打开, 1-新建窗口打开 */
+  // openMode?: 0 | 1;
+  // /** 未激活图标链接, 一般只设置在父节点 */
+  // icon?: string;
+  // /**
+  //  * 路径(不一定对应vue文件路径)
+  //  * @example 主应用：/usercenter
+  //  * @example 子应用：/admin/#/activity/list
+  //  * @example 外链：http://xxx.xxx.com/xxx
+  //  */
+  // path?: string;
+  // /**
+  //  * vue文件相对路径
+  //  * @example Layout
+  //  * @example /activity/list.vue
+  //  */
+  // componentStr?: string;
+  // /** 权限值, 多个逗号分隔 */
+  // authStr?: string;
+} & (
+  | {
+      /** 目标类型：0-主应用, 1-子应用, 2-外链, 3-父级菜单, 4-权限按钮 */
+      targetType: 0 | 1;
+      /**
+       * 路径(不一定对应vue文件路径)
+       * @example 主应用：/usercenter
+       * @example 子应用：/admin/#/activity/list
+       * @example 外链：http://xxx.xxx.com/xxx
+       */
+      path?: string;
+      /**
+       * vue文件相对路径
+       * @example Layout
+       * @example /activity/list.vue
+       */
+      componentStr?: string;
+    }
+  | {
+      /** 目标类型：0-主应用, 1-子应用, 2-外链, 3-父级菜单, 4-权限按钮 */
+      targetType: 2;
+      /** 外链打开方式: 0-本窗口打开, 1-新建窗口打开 */
+      openMode?: 0 | 1;
+      /**
+       * 路径(不一定对应vue文件路径)
+       * @example 主应用：/usercenter
+       * @example 子应用：/admin/#/activity/list
+       * @example 外链：http://xxx.xxx.com/xxx
+       */
+      path?: string;
+    }
+  | {
+      /** 目标类型：0-主应用, 1-子应用, 2-外链, 3-父级菜单, 4-权限按钮 */
+      targetType: 3;
+      /** 未激活图标链接, 一般只设置在父节点 */
+      icon?: string;
+    }
+  | {
+      /** 目标类型：0-主应用, 1-子应用, 2-外链, 3-父级菜单, 4-权限按钮 */
+      targetType: 4;
+      /** 权限值, 多个逗号分隔 */
+      authStr?: string;
+    }
+);
 
 /** 菜单列表数据类型 */
 export type MenuItemType = MenuOriginType & {
   /** 子菜单 */
   children?: Array<MenuItemType>;
-  // /** 外链地址，优先级会比`path`高(由url解析来的) */
-  // link: string;
   /**
    * 唯一索引`key`， 层数用`-`隔开
    * @example 0
    * @example 0-0
    * @example 1-0-1
    */
-  // key: string;
+  key?: string;
   /**
    * 额外状态值(不会保存到localStorage)
    */
