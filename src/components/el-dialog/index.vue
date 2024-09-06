733a5d1091780aaf4518df22c9a4a749ce46f0f4
陈永超 <chen_yongchao@126.com>
2024-09-06 14:54:28 +0800

    chore: el-dialog二次封装

---
+++ src/components/el-dialog/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-dialog
+    class="__el-dialog"
+    append-to-body
+  >
+    <el-scrollbar :max-height="props.maxHeight">
+      <slot></slot>
+    </el-scrollbar>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ElDialog } from 'element-plus';
+import 'element-plus/es/components/dialog/style/index';
+import { ElScrollbar } from 'element-plus';
+import 'element-plus/es/components/scrollbar/style/index';
+
+const props = defineProps({
+  maxHeight: {
+    type: String,
+    default: '80vh',
+  },
+});
+
+defineExpose({});
+</script>
+
+<style lang="scss">
+.__el-dialog {
+  margin: 0;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 6px;
+}
+</style>
