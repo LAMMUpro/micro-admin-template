e89c78c223e3c30d3da60ba6382127a5bced9700
陈永超 <chen_yongchao@126.com>
2024-09-29 17:13:47 +0800

    chore: MicroApp包一层解决重新渲染问题

---
+++ packages/micro-app-utils/vue3/MicroApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <MicroAppCore
+    v-bind="props"
+    :key="componentKey"
+    @_reloadApp="componentKey = Date.now()"
+  >
+    <!-- 处理插槽问题 -->
+    <template
+      v-for="(_, name) in $slots"
+      v-slot:[name]
+    >
+      <slot :name="name"></slot>
+    </template>
+  </MicroAppCore>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import MicroAppCore from './MicroAppCore.vue';
+import { MicroAppPropsType } from 'types';
+
+/** 处理props问题 */
+const props = defineProps<MicroAppPropsType>();
+
+const componentKey = ref(Date.now());
+</script>
