05b29c7f5576d151279377e425f403c93b523910
陈永超 <chen_yongchao@126.com>
2024-09-30 16:56:20 +0800

    chore: 引入lottie

---
+++ src/pages/demo/lottie.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="index">
+    <Vue3Lottie :animationData="animationData" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Vue3Lottie } from 'vue3-lottie';
+import animationData from '@/assets/lottie/Animation-1727686021645.json';
+</script>
+
+<style lang="scss" scoped></style>
