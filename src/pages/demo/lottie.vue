1384b007615a384aee8223040502784c98069819
陈永超 <chen_yongchao@126.com>
2024-10-08 15:12:33 +0800

    chore: lottie组件示例优化

--- src/pages/demo/lottie.vue
+++ src/pages/demo/lottie.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="index">
-    <Vue3Lottie :animation-link="getLottieJsonLink('close')" />
+    <Vue3Lottie
+      ref="lottieRef"
+      style="cursor: pointer"
+      :height="200"
+      :width="200"
+      :animation-link="animationLink"
+      :loop="false"
+      :auto-play="false"
+      @click="transformStatus"
+      @on-complete="animationEnd"
+    />
 
     <Vue3Lottie :animation-link="getLottieJsonLink('car')" />
   </div>
@@ -8,7 +18,25 @@
 
 <script lang="ts" setup>
 import { getLottieJsonLink } from '@/utils';
+import { computed } from 'vue';
+import { ref } from 'vue';
 import { Vue3Lottie } from 'vue3-lottie';
+
+const lottieRef = ref<InstanceType<typeof Vue3Lottie>>();
+
+const iconName = ref<'heart' | 'heart-fill'>('heart');
+
+const animationLink = computed(() => {
+  return getLottieJsonLink(iconName.value);
+});
+
+function transformStatus() {
+  lottieRef.value?.goToAndPlay(0);
+}
+
+function animationEnd() {
+  iconName.value = iconName.value === 'heart' ? 'heart-fill' : 'heart';
+}
 </script>
 
 <style lang="scss" scoped></style>
