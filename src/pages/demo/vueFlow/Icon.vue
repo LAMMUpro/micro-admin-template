4be33df44f4b8fa411bedc756d57a4b3fc0cc379
陈永超 <chen_yongchao@126.com>
2024-10-10 17:13:01 +0800

    chore: vueFlow示例

---
+++ src/pages/demo/vueFlow/Icon.vue
@@ -0,0 +1,53 @@
+<template>
+  <svg
+    v-if="name === '+'"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"
+    />
+  </svg>
+
+  <svg
+    v-else-if="name === '-'"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M19 13H5v-2h14v2z"
+    />
+  </svg>
+
+  <svg
+    v-else-if="name === '*'"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 12L6 6m6 6l6 6m-6-6l6-6m-6 6l-6 6"
+    />
+  </svg>
+
+  <svg
+    v-else-if="name === '/'"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M19 13H5v-2h14zm-7-8a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0 10a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2"
+    />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+defineProps(['name']);
+</script>
---
+++ src/pages/demo/vueFlow/ValueNode.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    :id="`${id}-input`"
+    v-model="value"
+    type="number"
+    class="nodrag"
+  />
+
+  <Handle
+    type="source"
+    :position="Position.Right"
+    :connectable="false"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { Handle, Position, useVueFlow } from '@vue-flow/core';
+
+const props = defineProps(['id', 'data']);
+
+const { updateNodeData } = useVueFlow();
+
+const value = computed({
+  get: () => props.data.value,
+  set: (value) => updateNodeData(props.id, { value }),
+});
+</script>
