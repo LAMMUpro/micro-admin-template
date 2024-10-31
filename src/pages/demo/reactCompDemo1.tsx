b9b8afe16513b167eba01c27ac556f602c75ec4a
陈永超 <chen_yongchao@126.com>
2024-10-31 14:37:39 +0800

    chore: react demo ts类型优化

--- src/pages/demo/reactCompDemo1.tsx
+++ src/pages/demo/reactCompDemo1.tsx
@@ -1,73 +1,86 @@
 import React, { useState, useEffect, useRef, RefObject } from 'react';
 
-const reactCompDemo = () => {
-
-  const divRef = useRef<HTMLDivElement | undefined>(null);
-
-  return (
-    <>
-      <div ref={divRef as any} style={{ height: '360px', position: 'relative' }}>
-        <Canvas divRef={divRef}></Canvas>
-      </div>
-    </>
-  );
+interface Position {
+  x: number;
+  y: number;
 }
 
+const reactCompDemo = () => {
+  const divRef = useRef<HTMLDivElement>(null);
+  return (
+    <div
+      ref={divRef}
+      style={{ height: '360px', position: 'relative' }}
+    >
+      <Canvas containerRef={divRef}></Canvas>
+    </div>
+  );
+};
 export default reactCompDemo;
 
-
-interface Position {
-  x: number
-  y: number
-}
-
-function Canvas({ divRef }: any) {
-  const pos1 = usePointerPosition(divRef);
+function Canvas(props: { containerRef: React.RefObject<HTMLDivElement> }) {
+  const pos1 = usePointerPosition(props.containerRef);
   const pos2 = useDelayedValue(pos1, 100);
   const pos3 = useDelayedValue(pos2, 200);
   const pos4 = useDelayedValue(pos3, 100);
   const pos5 = useDelayedValue(pos4, 50);
   return (
     <>
-      <Dot position={pos1} opacity={1} />
-      <Dot position={pos2} opacity={0.8} />
-      <Dot position={pos3} opacity={0.6} />
-      <Dot position={pos4} opacity={0.4} />
-      <Dot position={pos5} opacity={0.2} />
+      <Dot
+        position={pos1}
+        opacity={1}
+      />
+      <Dot
+        position={pos2}
+        opacity={0.8}
+      />
+      <Dot
+        position={pos3}
+        opacity={0.6}
+      />
+      <Dot
+        position={pos4}
+        opacity={0.4}
+      />
+      <Dot
+        position={pos5}
+        opacity={0.2}
+      />
     </>
   );
 }
 
-function Dot({ position, opacity }: { position: Position, opacity: number }) {
+function Dot({ position, opacity }: { position: Position; opacity: number }) {
   return (
-    <div style={{
-      position: 'absolute',
-      backgroundColor: 'pink',
-      borderRadius: '50%',
-      opacity,
-      transform: `translate(${position.x}px, ${position.y}px)`,
-      pointerEvents: 'none',
-      left: -20,
-      top: -20,
-      width: 40,
-      height: 40,
-    }} />
+    <div
+      style={{
+        position: 'absolute',
+        backgroundColor: 'pink',
+        borderRadius: '50%',
+        opacity,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+      }}
+    />
   );
 }
 
-function usePointerPosition(divRef: RefObject<HTMLDivElement | undefined>) {
+function usePointerPosition(containerRef: RefObject<HTMLDivElement>) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   useEffect(() => {
-
     function handleMove(e: any) {
       setPosition({ x: e.offsetX, y: e.offsetY });
     }
 
-    divRef.current?.addEventListener('pointermove', handleMove);
+    containerRef.current?.addEventListener('pointermove', handleMove);
 
     return () => {
-      divRef.current?.removeEventListener('pointermove', handleMove);
-    }
+      containerRef.current?.removeEventListener('pointermove', handleMove);
+    };
   }, []);
   return position;
 }
