1ea78aace1d9294a0216c00c694f0a66abb25036
陈永超 <chen_yongchao@126.com>
2024-12-20 17:35:32 +0800

    frame-less-ui组件库示例

--- src/pages/introduce.vue
+++ src/pages/introduce.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="introduce font-size-3.75">
     <section class="-m-card">
+      <h2 className="-m-title">frame-less-ui组件库</h2>
+      
+      <fl-paging-select
+        :id="info.id"
+        @update-id="info.id = $event.detail[0]"
+        :label="info.name"
+        @update-label="info.name = $event.detail[0]"
+        :api="api"
+        immediate
+        :optionSetting="{label: 'name', id: 'id'}"
+        style="width: 300px;"
+      >
+      </fl-paging-select>
+      <div class="mt-3">{{ info }}</div>
+    </section>
+
+    <section class="-m-card">
       <h2 className="-m-title">项目介绍</h2>
       <p>
         这是一个<b>后台模板</b>。
@@ -98,6 +115,42 @@
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router';
+import { reactive, ref } from 'vue';
+
+const info = reactive({
+  id: '4',
+  name: '上海华耀公司有限公司',
+})
+
+function api() {
+  function generateId() {
+    return 1 + Date.now().toString().slice(5) + Math.random().toString(36).substring(2);
+  }
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          pageSize: 10,
+          currentPage: 1,
+          totalCount: 100,
+          list: [
+            { id: generateId(), name: '上海外服（集团）有限公司国际人才分公司超长溢出测试' },
+            { id: generateId(), name: '上海电气集团股份有限公司' },
+            { id: generateId(), name: '上海外服（集团）有限公司' },
+            { id: generateId(), name: '上海华耀公司有限公司' },
+            { id: generateId(), name: '上海北泰实业股份有限公司' },
+            { id: generateId(), name: '上海国际集团有限公司' },
+            { id: generateId(), name: '上海明国实业有限公司' },
+            { id: generateId(), name: '上海曼珩鼎实业有限公司' },
+            { id: generateId(), name: '上海闻岱商业管理有限公司' },
+            { id: generateId(), name: '上海叁零财务管理咨询有限公司' },
+          ]
+        },
+        success: true
+      })
+    }, 2000)
+  })
+}
 
 const router = useRouter();
 </script>
